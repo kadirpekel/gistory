06c28f37a81bf89c3c201003fbf6e2c6886637d9
kadirpekel <kadirpekel@gmail.com>
2012-07-26 19:17:40 +0300

    modified local host and port bindings to work in conjunction with heroku

--- app.py
+++ app.py
@@ -29,4 +29,5 @@ def gist(gist_id):
   return render_template("gist.html", gist=gist)
 
 if __name__ == "__main__":
-  app.run()
+  port = int(os.environ.get('PORT', 5000))
+  app.run(host='0.0.0.0', port=port)
